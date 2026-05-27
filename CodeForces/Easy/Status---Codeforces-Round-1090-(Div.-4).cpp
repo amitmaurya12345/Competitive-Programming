1df70c6b6fe85ef2ff2424798f67d74d8dde431a
amitmaurya12345 <135938196+amitmaurya12345@users.noreply.github.com>
2026-05-27 22:35:20 +0530

    feat(codeforces): solve Status - Codeforces Round 1090 (Div. 4) [C++]

---
+++ CodeForces/Easy/Status---Codeforces-Round-1090-(Div.-4).cpp
@@ -0,0 +1,28 @@
+# Status - Codeforces Round 1090 (Div. 4)
+
+- Platform: CodeForces
+- URL: https://codeforces.com/contest/2218/my
+- Difficulty: Easy
+- Language: C++
+- Status: Accepted
+- Runtime: 31 ms
+- Memory: 100 KB
+- Solved At: 2026-05-27T17:05:53.397Z
+
+## Code
+```cpp
+#include<bits/stdc++.h> 
+using namespace std;
+ int main(){
+        int t;
+cin>>t;
+    while(t--){
+                int n;
+cin>>n;
+         cout<<(n)<<endl;
+    
+}
+        return 0;
+    
+}
+```
